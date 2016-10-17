9b0db197a25a52ddd0d75172ee60962e8eb42891
Eamonn O'Brien-Strain <e@obrain.com>
2016-10-16 22:36:34 -0700

    Convert to ES6 syntax

--- src/sekritweb.ts
+++ src/sekritweb.ts
@@ -3,59 +3,45 @@
 
 
 (() => {
-  var CIRKLE_CIPHER, CIRKLE_PREFIX, Cipher, afterTick, assert, createCirkle, dontHaveCirkle, fromHash, haveCirkle, main, require;
+  const CIRKLE_PREFIX = 'supersekrit';
 
-  CIRKLE_PREFIX = 'supersekrit';
-
-  Cipher = ((prefix, suffix, password) => {
-    var SEKRIT_PATT, crypt2sekrit, fromWebsafe, sekrit2crypt, toWebsafe;
-    SEKRIT_PATT = new RegExp(prefix + "([A-Za-z0-9_,-]{46,})" + suffix);
+  const Cipher = ((prefix, suffix, password) => {
+    var crypt2sekrit, fromWebsafe, sekrit2crypt, toWebsafe;
+    const SEKRIT_PATT = new RegExp(prefix + "([A-Za-z0-9_,-]{46,})" + suffix);
     toWebsafe = (s) => {
       s = s.replace(/\+/g, '-').replace(/\//g, '_');
       return "" + prefix + s + suffix;
     };
     fromWebsafe = (s) => {
-      var sekS;
-      sekS = SEKRIT_PATT.exec(s);
+      const sekS = SEKRIT_PATT.exec(s);
       if (sekS === null) {
         throw "Cannot decrypt: expect something of the form \"" + prefix + "..." + suffix + "\"";
       }
       return sekS[1].replace(/\-/g, '+').replace(/_/g, '/');
     };
     sekrit2crypt = (s) => {
-      var ct, iv, ref, salt;
-      ref = fromWebsafe(s).split(','), iv = ref[0], salt = ref[1], ct = ref[2];
-      assert(() => {
-        return iv.length === 22;
-      });
-      assert(() => {
-        return salt.length === 11;
-      });
-      assert(() => {
-        return ct.length >= 11;
-      });
+      const ref = fromWebsafe(s).split(',');
+      const iv = ref[0];
+      const salt = ref[1];
+      const ct = ref[2];
+      assert(() => iv.length === 22);
+      assert(() => salt.length === 11);
+      assert(() => ct.length >= 11);
       return "{iv:\"" + iv + "\",salt:\"" + salt + "\",ct:\"" + ct + "\"}";
     };
     crypt2sekrit = (c) => {
-      var commaSeparated;
-      commaSeparated = c.replace(/^{iv:\"/, '').replace(/",salt:"/, ',').replace(/",ct:"/, ',').replace(/\"}$/, '');
+      const commaSeparated = c.replace(/^{iv:\"/, '').replace(/",salt:"/, ',').replace(/",ct:"/, ',').replace(/\"}$/, '');
       return toWebsafe(commaSeparated);
     };
-    this.encrypt = (plaintext) => {
-      return crypt2sekrit(sjcl.encrypt(password, plaintext));
-    };
+    this.encrypt = (plaintext) => crypt2sekrit(sjcl.encrypt(password, plaintext));
     this.decrypt = (ciphertext) => {
-      require(() => {
-        return ciphertext;
-      });
-      require(() => {
-        return ciphertext.length >= 46;
-      });
+      require(() => ciphertext);
+      require(() => ciphertext.length >= 46);
       return sjcl.decrypt(password, sekrit2crypt(ciphertext));
     };
-    return null;
   });
 
+  let assert, require;
   if (window.location.href.slice(0, 5) === 'file:') {
     console.log('Test Environment. Assertions enabled');
     assert = (predicate) => {
@@ -73,98 +59,84 @@
     require = (predicate) => {};
   }
 
-  CIRKLE_CIPHER = new Cipher('O', '', 'supersekrit');
+  const CIRKLE_CIPHER = new Cipher('O', '', 'supersekrit');
 
   $(() => {
     $(window).on('hashchange', main);
     main();
     $('#create').click(() => {
-      var cirkleString, e, friendly;
       try {
-        friendly = $('#friendly').val().trim() || '(anonymous)';
-        cirkleString = createCirkle(friendly);
+        const friendly = $('#friendly').val().trim() || '(anonymous)';
+        const cirkleString = createCirkle(friendly);
         return window.location.hash = '#' + cirkleString;
-      } catch (_error) {
-        e = _error;
+      } catch (e) {
         return alert(e);
       }
     });
-    return $('textarea[readonly]').mouseenter(() => {
-      return $(this).select();
-    });
+    return $('textarea[readonly]').mouseenter(() => $(this).select());
   });
 
-  main = () => {
-    var $content, cirkleString;
-    $content = $('#content');
-    cirkleString = fromHash();
-    if (!cirkleString || cirkleString.length === 0) {
-      return dontHaveCirkle($content);
-    } else {
-      return haveCirkle($content, cirkleString);
-    }
+  const main = () => {
+    const $content = $('#content');
+    const cirkleString = fromHash();
+    return (!cirkleString || cirkleString.length === 0)
+        ? dontHaveCirkle($content)
+        : haveCirkle($content, cirkleString);
   };
 
-  dontHaveCirkle = ($content) => {
+  const dontHaveCirkle = ($content) => {
     $('#no-circkle').slideDown();
     $('#bad-circkle').slideUp();
     return $('#have-circkle').slideUp();
   };
 
-  haveCirkle = ($content, cirkleString) => {
-    var $msgIn, $sekritIn, cirkle, e, friendly, prefix, ref;
+  const haveCirkle = ($content, cirkleString) => {
     $('.cirkle-name').text(cirkleString);
+    let prefix, friendly;
     try {
-      ref = (CIRKLE_CIPHER.decrypt(cirkleString)).split('|'), prefix = ref[0], friendly = ref[1];
-    } catch (_error) {
-      e = _error;
+      const ref = (CIRKLE_CIPHER.decrypt(cirkleString)).split('|')
+      prefix = ref[0]
+      friendly = ref[1];
+    } catch (e) {
       prefix = e;
+      friendly = "ERROR decrypting";
     }
     $('#no-circkle').slideUp();
     if (prefix !== CIRKLE_PREFIX) {
       $('#bad-circkle').slideDown();
       return $('#have-circkle').slideUp();
-    } else {
-      $('title').text("Sekrit Cirkle: " + friendly);
-      $('.friendly-name').text(friendly);
-      $('#cirkle-link').val("Go to this web page to receive messages from the Sekrit Cirkle: " + friendly + "\n" + window.location.href);
-      $('#bad-circkle').slideUp();
-      $('#have-circkle').slideDown();
-      cirkle = new Cipher('Shh:', '!', cirkleString);
-      $msgIn = $('#msg-in');
-      $msgIn.keyup(() => {
-        return afterTick(() => {
-          $('#sekrit-out').text(cirkle.encrypt($msgIn.val().trim()));
-          return $('#secret-out-wrapper').slideDown();
-        });
-      });
-      $sekritIn = $('#sekrit-in');
-      return $sekritIn.on('paste', () => {
-        return afterTick(() => {
-          var sekrit;
-          sekrit = $sekritIn.val().trim();
-          try {
-            $('#msg-out').text(cirkle.decrypt(sekrit));
-            $('#msg-out-wrapper').slideDown();
-          } catch (_error) {
-            e = _error;
-            alert(e);
-          }
-        });
-      });
     }
+    $('title').text("Sekrit Cirkle: " + friendly);
+    $('.friendly-name').text(friendly);
+    $('#cirkle-link').val("Go to this web page to receive messages from the Sekrit Cirkle: " + friendly + "\n" + window.location.href);
+    $('#bad-circkle').slideUp();
+    $('#have-circkle').slideDown();
+    const cirkle = new Cipher('Shh:', '!', cirkleString);
+    const $msgIn = $('#msg-in');
+    $msgIn.keyup(() => {
+      return afterTick(() => {
+        $('#sekrit-out').text(cirkle.encrypt($msgIn.val().trim()));
+        return $('#secret-out-wrapper').slideDown();
+      });
+    });
+    const $sekritIn = $('#sekrit-in');
+    return $sekritIn.on('paste', () => {
+      return afterTick(() => {
+        const sekrit = $sekritIn.val().trim();
+        try {
+          $('#msg-out').text(cirkle.decrypt(sekrit));
+          $('#msg-out-wrapper').slideDown();
+        } catch (e) {
+          alert(e);
+        }
+      });
+    });
   };
 
-  fromHash = () => {
-    return window.location.hash.substring(1);
-  };
+  const fromHash = () => window.location.hash.substring(1);
 
-  createCirkle = (friendly) => {
-    return CIRKLE_CIPHER.encrypt(CIRKLE_PREFIX + "|" + friendly);
-  };
+  const createCirkle = (friendly) => CIRKLE_CIPHER.encrypt(CIRKLE_PREFIX + "|" + friendly);
 
-  afterTick = (func) => {
-    return setTimeout(func, 0);
-  };
+  const afterTick = (func) => setTimeout(func, 0);
 
 }).call(this);
