f3a62c525fb4d6d33375b10104c89fbc2d97d93a
Eamonn O'Brien-Strain <e@obrain.com>
2016-10-18 22:43:03 -0700

    Add missing copyright notice

--- src/sekritweb.ts
+++ src/sekritweb.ts
@@ -1,3 +1,13 @@
+// Copyright (c) 2014 Eamonn O'Brien-Strain All rights reserved. This
+// program and the accompanying materials are made available under the
+// terms of the Eclipse Public License v1.0 which accompanies this
+// distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+//
+// Contributors:
+// Eamonn O'Brien-Strain e@obrain.com - initial author
+
+
 /// <reference path="../build/node_modules/sjcl-typescript-definitions/sjcl/sjcl.d.ts" />
 /// <reference path="../build/node_modules/@types/jquery/index.d.ts" />
 
